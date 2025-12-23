06bbfe7a31d8780665d6de62eaa0a1e2b68fcf6f
Khasiyev28 <mahirkhasiyev28@gmail.com>
2025-12-23 13:25:13 +0400

    .

---
+++ Seminar/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace Seminar
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            pictureBox1 = new PictureBox();
+            pictureBox2 = new PictureBox();
+            button1 = new Button();
+            label1 = new Label();
+            panel1 = new Panel();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            panel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BorderStyle = BorderStyle.FixedSingle;
+            panel1.Controls.Add(pictureBox1); // Oyunçu panelin daxilinə keçirildi
+            panel1.Controls.Add(pictureBox2); // Sümük panelin daxilindədir
+            panel1.Location = new Point(42, 22);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(683, 427);
+            panel1.TabIndex = 4;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(10, 10);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(60, 60);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Image = (Image)resources.GetObject("pictureBox2.Image");
+            pictureBox2.Location = new Point(433, 93);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(80, 40);
+            pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox2.TabIndex = 1;
+            pictureBox2.TabStop = false;
+            // 
+            // button1
+            // 
+            button1.Cursor = Cursors.Hand;
+            button1.Location = new Point(474, 470);
+            button1.Name = "button1";
+            button1.Size = new Size(100, 38);
+            button1.TabIndex = 2;
+            button1.Text = "New Game";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            button1.TabStop = false; // Fokusun burada qalmaması üçün
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(139, 477);
+            label1.Name = "label1";
+            label1.Size = new Size(92, 31);
+            label1.TabIndex = 3;
+            label1.Text = "Score: 0";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(760, 520);
+            Controls.Add(label1);
+            Controls.Add(button1);
+            Controls.Add(panel1);
+            Name = "Form1";
+            Text = "Catch the Bone Game";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            panel1.ResumeLayout(false);
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private PictureBox pictureBox1;
+        private PictureBox pictureBox2;
+        private Button button1;
+        private Label label1;
+        private Panel panel1;
+    }
+}
---
+++ Seminar/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Seminar
+{
+    public partial class Form1 : Form
+    {
+        int speed = 10;
+        int score = 0;
+        Random rnd = new Random();
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.Load += Form1_Load;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label1.Text = "Score: 0";
+
+            panel1.TabStop = false;
+            pictureBox1.TabStop = false;
+            pictureBox2.TabStop = false;
+            button1.TabStop = false;
+
+            NewGame();
+        }
+
+        protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
+        {
+            if (keyData == Keys.A || keyData == Keys.Left)
+                pictureBox1.Left -= speed;
+            else if (keyData == Keys.D || keyData == Keys.Right)
+                pictureBox1.Left += speed;
+            else if (keyData == Keys.W || keyData == Keys.Up)
+                pictureBox1.Top -= speed;
+            else if (keyData == Keys.S || keyData == Keys.Down)
+                pictureBox1.Top += speed;
+
+            CheckPanelBorder();
+            CheckCollision();
+
+            return base.ProcessCmdKey(ref msg, keyData);
+        }
+
+        void CheckPanelBorder()
+        {
+            if (pictureBox1.Left < 0 ||
+                pictureBox1.Top < 0 ||
+                pictureBox1.Right > panel1.Width ||
+                pictureBox1.Bottom > panel1.Height)
+            {
+                MessageBox.Show("Panelin borderinə dəydin! Uduzdun!");
+                NewGame();
+            }
+        }
+
+        void CheckCollision()
+        {
+            if (pictureBox1.Bounds.IntersectsWith(pictureBox2.Bounds))
+            {
+                score++;
+                label1.Text = "Score: " + score;
+                MoveBone();
+            }
+        }
+
+        void MoveBone()
+        {
+            pictureBox2.Left = rnd.Next(0, panel1.Width - pictureBox2.Width);
+            pictureBox2.Top = rnd.Next(0, panel1.Height - pictureBox2.Height);
+        }
+
+        void NewGame()
+        {
+            score = 0;
+            label1.Text = "Score: 0";
+            pictureBox1.Left = 50;
+            pictureBox1.Top = 50;
+            MoveBone();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            NewGame();
+        }
+    }
+}
